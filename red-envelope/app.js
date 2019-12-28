b4674d539188fdb1b77577b947c1fa13d6b8f691
hjrugk <tassjjjj@live.cn>
2019-12-28 11:14:43 +0800

    从服务器请求红包

--- red-envelope/app.js
+++ red-envelope/app.js
@@ -6,17 +6,27 @@ window.onload = function() {
   // let info = this.document.querySelector(".wrapper .inner .envelope .hidden .info")
   let resetBtn = this.document.querySelector(".wrapper .inner .envelope .reset")
   money.onceClick = false
-  money.addEventListener("click", () => {
-    let account = (Math.random() * 10).toFixed(2)
+  money.addEventListener("click", (e) => {
+    e.preventDefault()
+    let xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function(e) {
+      if(xhr.readyState === 4 && xhr.status === 200) {
+        let account = xhr.responseText
+        moneyAccount.innerHTML = account
+      }
+    }
+    xhr.open("GET", "http://127.0.0.1:3000/m", true)
+    xhr.send(null)
+    // let account = (Math.random() * 10).toFixed(2)
     if(!money.onceClick) {
       money.classList.remove("reverse")
       money.classList.add("reverse")
       money.onceClick = true
       content.classList.remove("opened")
       content.classList.add("opened")
-      moneyAccount.innerHTML = account
       resetBtn.style.opacity = "1"
       money.style.opacity = "0"
+      money.style.transform = "scale(1.3)"
       cover.style.boxShadow = "0px 0px 0px #333"
       money.style.boxShadow = "0px 0px 0px #333"
     }
@@ -29,6 +39,7 @@ window.onload = function() {
     resetBtn.style.opacity = "0"
     content.classList.remove("opened")
     money.style.opacity = "1"
+    money.style.transform = "scale(1)"
     cover.style.boxShadow = "0px 10px 0px #333"
   }
 }
---
+++ server/index.js
@@ -0,0 +1,47 @@
+const http = require("http")
+const fs = require("fs")
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'application/json;charset=utf-8',
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Origin': '*' //可以是*，也可以是跨域的地址
+  })
+})
+
+server.on("request", (req, res) => {
+  let totalMoney
+  let randomMoney
+  let sumMoney
+  if (req.url === "/m") {
+    fs.readFile("./data.json", async (err, data) => {
+      totalMoney = await JSON.parse(data).total
+      sumMoney = await JSON.parse(data).sum
+      if (sumMoney >= 100) {
+        sumMoney = 0
+      }
+      if (totalMoney < 10) {
+        randomMoney = totalMoney
+        totalMoney = await 100
+      } else {
+        randomMoney = (Math.random() * 10).toFixed(2)
+        totalMoney = await (totalMoney - randomMoney).toFixed(2)
+      }
+      sumMoney = await sumMoney + parseFloat(randomMoney)
+      fs.writeFile("./data.json", JSON.stringify({
+        total: totalMoney,
+        sum: sumMoney
+      }), () => {
+        console.log(totalMoney, sumMoney)
+      })
+
+      res.write(randomMoney)
+      res.end()
+    })
+  }
+})
+
+server.listen(3000, () => {
+  fs.writeFile("./data.json", JSON.stringify({total:"100",sum:0}),() => {
+    console.log("连接成功")
+  })
+})
